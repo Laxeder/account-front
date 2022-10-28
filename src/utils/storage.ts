3fa6c8d3dbde056688bbf7e05a0265dcfa468570
Laxeder <Laxederbr@gmail.com>
2022-10-28 20:04:06 -0300

    resolve bug

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,45 +1,45 @@
-import { Observable, BehaviorSubject, } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 export class Storage {
-
-  // notifica o loading 
-  private _isLoading$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  // notifica o loading
+  private _isLoading$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
+    false
+  );
   public get isLoading$(): Observable<boolean> {
     return this._isLoading$.asObservable();
   }
 
   public setItem(key: string, value: any): boolean {
     try {
-      if(!!!key || typeof key !== 'string') {
-        return  false;
+      if (!!!key || typeof key !== 'string') {
+        return false;
       }
 
       const strValue: string = JSON.stringify(value);
 
-      if(!!!strValue || typeof strValue !== 'string') {
-        return  false;
+      if (!!!strValue || typeof strValue !== 'string') {
+        return false;
       }
 
       localStorage.setItem(key, strValue);
 
-      return true
+      return true;
     } catch {
-      return false
+      return false;
     }
   }
 
   public getItem(key: string): any {
     try {
-
-      if(!!!key || key !== 'string') {
+      if (!!!key || typeof key !== 'string') {
         return <any>{};
-      };
+      }
 
       const value: string = localStorage.getItem(key) || '';
 
-      if(!!!value) {
+      if (!!!value) {
         return <any>{};
-      };
+      }
 
       return <any>JSON.parse(value);
     } catch {
@@ -49,17 +49,15 @@ export class Storage {
 
   public removeItem(key: string): boolean {
     try {
-
-      if(!!!key || typeof key !== 'string') {
+      if (!!!key || typeof key !== 'string') {
         return false;
-      };
+      }
 
       localStorage.removeItem(key);
 
       return true;
-
     } catch {
       return false;
-    };
+    }
   }
 }
